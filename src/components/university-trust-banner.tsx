afc1642a14276ca4885260e68fb82d232bdfed8a
Minhaz EsmAil <minhazesmail@gmail.com>
2026-08-27 20:06:14 +0600

    Fix remaining university logo backgrounds and IUB source

--- src/components/university-trust-banner.tsx
+++ src/components/university-trust-banner.tsx
@@ -3,51 +3,61 @@ const universities = [
     name: 'North South University',
     shortName: 'NSU',
     logo: 'https://image.pngaaa.com/679/7881679-middle.png',
+    knockOutWhite: true,
   },
   {
     name: 'BRAC University',
     shortName: 'BRACU',
     logo: 'https://www.bracu.ac.bd/sites/default/files/resources/media/bracu_logo.png',
+    knockOutWhite: false,
   },
   {
     name: 'American International University-Bangladesh',
     shortName: 'AIUB',
     logo: 'https://vectorseek.com/wp-content/uploads/2023/08/Aiub-Logo-Vector.svg-.png',
+    knockOutWhite: false,
   },
   {
     name: 'Independent University, Bangladesh',
     shortName: 'IUB',
-    logo: 'https://iub.ac.bd/_next/image?q=75&url=https%3A%2F%2Fiub.ac.bd%2Fmedia-backend%2Fmedia%2Fiub-logo_1_png_color-9303cd91-58be-47a4-97f7-0f1231a850be.png&w=3840',
+    logo: 'https://images.seeklogo.com/logo-png/61/1/independent-university-bangladesh-iub-logo-png_seeklogo-619669.png',
+    knockOutWhite: true,
   },
   {
     name: 'United International University',
     shortName: 'UIU',
     logo: 'https://bitm.org.bd/storage/collaborator/vfeSmq8C64ZLUVOmzUhq6HJUiunqQt2Uqgk2WYBh.png',
+    knockOutWhite: false,
   },
   {
     name: 'Daffodil International University',
     shortName: 'DIU',
     logo: 'https://icon2.cleanpng.com/20180505/bvw/avstwg208.webp',
+    knockOutWhite: true,
   },
   {
     name: 'University of Dhaka',
     shortName: 'DU',
     logo: 'https://w7.pngwing.com/pngs/520/105/png-transparent-dhaka-university-library-institute-of-information-technology-university-of-dhaka-institute-of-business-administration-university-of-dhaka-curzon-hall-alumni-association-emblem-people-logo.png',
+    knockOutWhite: true,
   },
   {
     name: 'University of Chittagong',
     shortName: 'CU',
     logo: 'https://cu.ac.bd/wp-content/uploads/2024/03/university-of-chittagong-seeklogo.com-removebg-preview-removebg-preview-1.png',
+    knockOutWhite: false,
   },
   {
     name: 'Green University of Bangladesh',
     shortName: 'GUB',
     logo: 'https://images.seeklogo.com/logo-png/65/1/green-university-of-bangladesh-logo-png_seeklogo-653144.png',
+    knockOutWhite: true,
   },
   {
     name: 'East West University',
     shortName: 'EWU',
     logo: 'https://upload.wikimedia.org/wikipedia/commons/1/16/East-west-university-LogoSVG.svg',
+    knockOutWhite: false,
   },
 ] as const
 
@@ -70,7 +80,7 @@ export default function UniversityTrustBanner() {
                 title={university.name}
                 loading="lazy"
                 referrerPolicy="no-referrer"
-                className="h-16 w-full object-contain opacity-80 grayscale transition group-hover:opacity-100 group-hover:grayscale-0"
+                className={`h-16 w-full object-contain opacity-80 grayscale transition group-hover:opacity-100 group-hover:grayscale-0 ${university.knockOutWhite ? 'mix-blend-multiply dark:mix-blend-normal' : ''}`}
               />
               <p className="mt-3 text-[11px] font-semibold leading-4 text-muted transition group-hover:text-main">
                 {university.name}
